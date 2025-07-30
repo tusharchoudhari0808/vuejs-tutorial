b8523269eb077eb6ef4ef92ba0935eab630be556
Tushar Choudhari <tusharchoudhari0808@gmail.com>
2025-07-30 16:22:19 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Counter from './components/Counter.vue';
+
+</script>
+
+<template>
+  <h1 class="h">Tushar CFhoudhari</h1>
+  
+<Counter/>
+
+</template>
+
+<style scoped>
+.h{
+background-color: aqua;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<script setup >
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const incriment = () =>{
+  count.value++;
+}
+</script>
+
+
+<template>
+<div>
+    <h1>Counter </h1>
+    <p>count: {{ count }}</p>
+    <button @click ="incriment"> + </button>
+</div>
+</template>
+
+
+<style>
+
+</style>
